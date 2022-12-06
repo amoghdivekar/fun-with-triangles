084ad240ffbbf1cddb5b1c942015c346a21de0c2
Amogh Divekar <amoghdivekar22@gmail.com>
2022-12-06 13:08:13 +0530

    update: area of triangle

--- area.js
+++ area.js
@@ -1,32 +1,19 @@
-const areaSides = document.querySelector(".side-input");
+const base = document.querySelector("#base");
+const height = document.querySelector("#height");
 const areaBtn = document.querySelector("#area-btn");
 const areaOutput = document.querySelector("#output");
 
-function calculateAreaOfTriangle() {
-    if(areaSides.value === "") {
-        areaOutput.innerText = "Please enter both the fields";
-    } else if(Number(areaSides.value <= 0)) {
-        areaOutput.innerText = "Both values should be greater than zero";
-    } else {
-        calculateArea(Number(areaSides.value), Number(areaSides.value));
-    }
+function calculateArea(a, b) {
+    var area = (a * b) / 2;
+    areaOutput.innerText = "The area of tiangle is " + area.toFixed(2);
 }
 
-function calculateArea(base, height) {
-    const areaOfTriangle = (base * height) / 2;
-    // return areaOfTriangle;
-    areaOutput.innerText = "The area of tiangle is " + areaOfTriangle;
+function areaOfTriangle() {
+    if (base.value === "" || height.value === "")
+      output.innerText = "Please enter both values";
+    else if (Number(base.value <= 0) || Number(height.value <= 0))
+      output.innerText = "Both values should be greater than zero";
+    else calculateArea(Number(base.value), Number(height.value));
 }
 
-// function calculateAreaOfTriangle() {
-//     const areaOfTriangle = areaOfTriangle(Number(areaSides[0].value), Number(areaSides[1].value));
-//     areaOutput.innerText = "The area of tiangle is " + areaOfTriangle;
-// }
-
-// console.log(calculateArea);
-
-// function clickHandler() {
-//     console.log(calculateArea);
-// }
-
-areaBtn.addEventListener("click", calculateAreaOfTriangle);
+areaBtn.addEventListener("click", areaOfTriangle);
