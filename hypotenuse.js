f84f746ebec3c54883c71d79d8c08b8f02164385
Amogh Divekar <amoghdivekar22@gmail.com>
2022-12-20 06:58:09 +0530

    Update hypotenuse.js

--- hypotenuse.js
+++ hypotenuse.js
@@ -10,7 +10,7 @@ function calculateSumOfSquares(a, b) {
 function calculateHypotenuse() {
     const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
     const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-    outputEl.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+    outputEl.innerText = "The length of hypotenuse is " + lengthOfHypotenuse.toFixed(2);
 }
 
 hypotenuseBtn.addEventListener("click", calculateHypotenuse);
